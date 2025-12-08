cdaf929780de9e80d6d1dc16b4b975c2ec45fe06
Alexis Rodriguez <alexisrodriguezdev@gmail.com>
2025-12-07 22:09:15 -0600

    chore: add test cases pre tested

--- src/test/java/com/ecommerce/common/auth/JwtUtilsTest.java
+++ src/test/java/com/ecommerce/common/auth/JwtUtilsTest.java
@@ -1,30 +1,31 @@
 package com.ecommerce.common.auth;
 
 import io.jsonwebtoken.Claims;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class JwtUtilsTest {
 
-    private JwtUtils jwtUtils;
-    private SecretKey secretKey;
-    private Clock clock;
+    private static JwtUtils jwtUtils;
+    private static SecretKey secretKey;
+    private static Clock clock;
 
     private final String subject = "user@example.com";
     private final List<String> roles = List.of("ADMIN", "USER");
 
-    @BeforeEach
-    void setup () throws Exception {
+    @BeforeAll
+    static void setup() throws Exception {
         secretKey = KeyGenerator.getInstance("HmacSHA256").generateKey();
         clock = Clock.fixed(
                 Instant.parse("2025-01-01T00:00:00Z"),
@@ -39,19 +40,24 @@ class JwtUtilsTest {
         String token = jwtUtils.generateToken(subject, roles);
         Claims claims = jwtUtils.parseClaims(token);
         assertTrue(jwtUtils.isTokenValid(claims, subject));
-
     }
 
     @Test
     void shouldDetectExpiredToken() {
+        // Token created at 00:00, expires at 01:00
         String token = jwtUtils.generateToken(subject, roles);
         Claims claims = jwtUtils.parseClaims(token);
-        // Move clock ahead by 2 hours
+
+        // Simulate current time at 02:00
         Clock expiredClock = Clock.fixed(
                 Instant.parse("2025-01-01T02:00:00Z"),
                 ZoneId.of("UTC")
         );
+
+        // JwtUtils with simulated time set to 02:00 (makes the token expired)
         JwtUtils jwtExpired = new JwtUtils(secretKey, 3600000, expiredClock);
+
+        // Should now be considered expired
         assertTrue(jwtExpired.isExpired(claims));
     }
 
@@ -63,21 +69,55 @@ class JwtUtilsTest {
     }
 
     @Test
-    void shouldExtractRolesFromToken() {}
+    void shouldExtractRolesFromToken() {
+        String token = jwtUtils.generateToken(subject, roles);
+        Claims claim = jwtUtils.parseClaims(token);
+        assertEquals(roles, jwtUtils.extractRoles(claim));
+    }
 
     @Test
-    void shouldReturnExpirationDateFromToken() {}
+    void shouldReturnExpirationDateFromToken() {
+        String token = jwtUtils.generateToken(subject, roles);
+        Claims claim = jwtUtils.parseClaims(token);
+        Date actualDate = jwtUtils.extractExpiration(claim);
+        Date expectedDate  = Date.from(
+                Instant.parse("2025-01-01T00:00:00Z")
+                        .plusMillis(3600000)
+        );
+        assertEquals(actualDate , expectedDate);
+    }
 
     @Test
-    void shouldFailValidation_WhenTokenIsMalformed() {}
+    void shouldFailValidation_WhenTokenIsMalformed() {
+        String malformedToken = "asdaas123sasdtrash";
+        Claims claims = jwtUtils.parseClaims(malformedToken);
+        assertFalse(jwtUtils.isTokenValid(claims,subject));
+    }
 
     @Test
-    void shouldFailValidation_WhenSignatureIsInvalid() {}
+    void shouldFailValidation_WhenSignatureIsInvalid() throws NoSuchAlgorithmException {
+        String token = jwtUtils.generateToken(subject, roles);
+        Claims claim = jwtUtils.parseClaims(token);
+
+        // Generate a completely different key
+        SecretKey invalidKey = KeyGenerator.getInstance("HmacSHA256").generateKey();
+
+        // JwtUtils with wrong key
+        JwtUtils jwtWithWrongKey = new JwtUtils(invalidKey, 3600000, clock);
+        assertFalse(jwtWithWrongKey.isTokenValid(claim, subject));
+    }
 
     @Test
-    void shouldValidateToken_WhenTokenIsValid() {}
-
-    @Test
-    void shouldNotValidateToken_WhenTokenIsExpired() {}
+    void shouldNotValidateToken_WhenTokenIsExpired() {
+        String token = jwtUtils.generateToken(subject, roles);
+        Claims claims = jwtUtils.parseClaims(token);
+        // Move clock ahead by 2 hours
+        Clock expiredClock = Clock.fixed(
+                Instant.parse("2025-01-01T02:00:00Z"),
+                ZoneId.of("UTC")
+        );
+        JwtUtils jwtExpired = new JwtUtils(secretKey, 3600000, expiredClock);
+        assertFalse(jwtExpired.isTokenValid(claims,subject));
+    }
 
 }
